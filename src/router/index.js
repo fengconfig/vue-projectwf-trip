1b0d7a2d1bb36536a54db73d6b32366d85896e18
feng.wei <hello@qq.com>
2025-07-31 23:46:11 +0800

    引入router

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+    history: createWebHashHistory,
+    // 映射关系： path -> component
+    routes: [
+        
+    ]
+})
+
+export default router;
