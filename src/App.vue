f5ec7913fb99cd46f8e3c08e7ca1789f28220ad9
feng.wei <hello@qq.com>
2025-08-07 23:09:05 +0800

    loading加载

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@
     <router-view />
     <!-- 底部导航栏 -->
     <tab-bar v-if="!$route.meta.hideTabBar" />
+    <loading></loading>
   </div>
 </template>
 
 <script setup>
 
 import TabBar from "@/components/tab-bar/tab-bar.vue"
-
+import loading from "./components/loading/loading.vue";
 
 </script>
 
---
+++ src/components/loading/loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="loading">
+    <div class="bg">
+      <img src="@/assets/img/home/full-screen-loading.gif" alt="">
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.loading {
+  display: flex;
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  img {
+    height: 30px;
+    width: 30px;
+  }
+}
+</style>
